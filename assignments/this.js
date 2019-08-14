5eded199fea4b1715374826ff4339ea6d73d7866
Brian Hague <alphaseinor@gmail.com>
2019-08-13 23:08:05 -0500

    added prototypes

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. WindowBinding, it's the this for the web browser (don't touch!)
+* 2. Implicit Binding, directly binds to the object it's "dotted" to
+* 3. New Binding, Makes a new
+* 4. Explicit binding, forces a function call to use an object without reference
 *
 * write out a code example of each explanation above
 */
@@ -24,24 +24,30 @@ console.log(windowBinding())
 
 // code example for Implicit Binding
 
-let ImplicitBinding = function () {
-  name = 'ImplicitBinding';
+let implicitBinding = {
+  name : 'implicitBinding',
+  doSomething : function(something){
+    console.log(this.name + ' is ' + something);
+  } 
 }
 
-ImplicitBinding.prototype.doSomething = (something) => {
-  console.log(this.name + ' is ' + something);
-}
-
-let obj = new ImplicitBinding();
-
-obj.doSomething('meh'); // prints 'implicitBinding is meh'
+implicitBinding.doSomething('meh'); // prints 'implicitBinding is meh'
 
 // Principle 3
 
 // code example for New Binding
+let coder = {
+  name: "brian",
+  activity: "coding"
+}
 
+let emotions = [ "tired", "working", "coding"] 
+
+implicitBinding.doSomething.apply(coder, emotions)
 
 
 // Principle 4
 
 // code example for Explicit Binding
+
+implicitBinding.doSomething.call(coder, coder.activity)
