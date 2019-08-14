867562ae4c44b16eb43c46d4a5de175084748f2c
Brian Hague <alphaseinor@gmail.com>
2019-08-13 21:42:38 -0500

    added windowBinding and implicitBinding

--- assignments/this.js
+++ assignments/this.js
@@ -13,13 +13,34 @@
 
 // code example for Window Binding
 
+let windowBinding = () => {
+  return this;
+}
+
+console.log(windowBinding())
+
+
 // Principle 2
 
 // code example for Implicit Binding
 
+let ImplicitBinding = function () {
+  name = 'ImplicitBinding';
+}
+
+ImplicitBinding.prototype.doSomething = (something) => {
+  console.log(this.name + ' is ' + something);
+}
+
+let obj = new ImplicitBinding();
+
+obj.doSomething('meh'); // prints 'implicitBinding is meh'
+
 // Principle 3
 
 // code example for New Binding
+
+
 
 // Principle 4
 
